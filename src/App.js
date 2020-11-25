b1aa535f2f6a115c48cf6ff720e95508d6966166
Geoff Andrews <geoffrey.tsugio@gmail.com>
2020-11-25 16:06:43 -0500

    New, refined data added as source.

--- src/App.js
+++ src/App.js
@@ -35,8 +35,11 @@ export default class App extends Component {
       'esri/tasks/QueryTask',
       'esri/tasks/support/Query',
       'esri/widgets/Search',
-      'esri/widgets/LayerList'])
-      .then(([Map, MapView, Home, BasemapToggle, FeatureLayer, ImageryLayer, TextContent, AttachmentsContent, QueryTask, Query, Search, LayerList]) => {
+      'esri/widgets/LayerList',
+      'esri/widgets/Expand'])
+      .then(([Map, MapView, Home, BasemapToggle, FeatureLayer, ImageryLayer, TextContent, AttachmentsContent, QueryTask, Query, Search, LayerList, Expand]) => {
+        let photoLayerView;
+
         const map = new Map({
           basemap: 'topo-vector',
         });
@@ -58,11 +61,30 @@ export default class App extends Component {
           }
         });
 
+        const envelopesLayer = new FeatureLayer({
+          url:
+            "http://madgic.trentu.ca/arcgis/rest/services/airphoto2020/Airphoto2020/MapServer/3",
+            outFields: ["*"],
+            title: "Generalized Footprint",
+            opacity: 0.65,
+            maxScale: 6000000,
+            listMode: "hide",
+            popupEnabled: false
+        });
+        map.add(envelopesLayer, 0);
+        envelopesLayer.load().then(function(){
+          console.log("Envelopes loaded successfully.");
+        });
+
         const footprintsLayer = new FeatureLayer({
           url:
-            "http://madgic.trentu.ca/arcgis/rest/services/NAPLIndex/NAPLIndex/MapServer/2",
+            "http://madgic.trentu.ca/arcgis/rest/services/airphoto2020/Airphoto2020/MapServer/2",
             outFields: ["*"],
-            title: "Generalized Footprint"
+            title: "Refined Footprint",
+            opacity: 0.65,
+            minScale: 6000000,
+            maxScale: 150000,
+            popupEnabled: false
         });
         map.add(footprintsLayer, 0);
         footprintsLayer.load().then(function(){
@@ -71,7 +93,7 @@ export default class App extends Component {
 
         const flightLineLayer = new FeatureLayer({
           url:
-          "http://madgic.trentu.ca/arcgis/rest/services/NAPLIndex/NAPLIndex/MapServer/1",
+          "http://madgic.trentu.ca/arcgis/rest/services/airphoto2020/Airphoto2020/MapServer/1",
           outFields: ["YEAR", "ROLL", "PROVINCE"],
           opacity: 0.75,
           popupTemplate: rollTemplate,
@@ -88,7 +110,7 @@ export default class App extends Component {
         const textElement = new TextContent();
         const photoContent = 
           textElement.text = 
-            "<table class='esri-widget__table'><tr><th>Roll</th><td>{ROLL}</td></tr><tr><th>Photo</th><td>{PHOTO}</td></tr><tr><th>Capture Date</th><td>{NAPL_DATE}</td></tr><tr><th>Line Number</th><td>{LINE_NO}</td></tr><tr><th>Centre (<i>WGS 1984</i>)</th><td>{CNTR_LAT}, {CNTR_LON}</td></tr><tr><th>Altitude (<i>ft</i>)</th><td>{ALTITUDE}</td></tr><tr><th>Scale (<i>relative</i>)</th><td>{SCALE}</td></tr><tr><th>Camera</th><td>{CAMERA}</td></tr><tr><th>Lens</th><td>{LENS_NAME}</td></tr><tr><th>Focal Length (<i>mm</i>)</th><td>{FOCAL_LEN}</td></tr><tr><th>Film Type</th><td>{FILM_TYPE}</td></tr><tr><th>Film size (<i>mm</i>)</th><td>{FILM_SIZE}</td></tr><tr><th>Photo Dimensions (<i>in</i>)</th><td>{WIDTH} x {HEIGHT}</td></tr><tr><th>Box</th><td>{BOX}</td></tr><tr><th>File Name</th><td>{PHOTOID}</td></tr><tr style='display:none;'><th>Photo Year</th><td>{Year_}</td></tr><tr style='display:none;'><th>Raster ID</th><td>{RASTERID}</td></tr><tr style='display:none;'><th>Download URL</th><td>{DownloadURL}</td></tr></table>"
+            "<table class='esri-widget__table'><tr><th>Roll</th><td>{ROLL}</td></tr><tr><th>Photo</th><td>{PHOTO}</td></tr><tr><th>Capture Date</th><td>{NAPL_DATE}</td></tr><tr><th>Line Number</th><td>{LINE_NO}</td></tr><tr><th>Collection</th><td>{Collection}</td></tr><tr><th>Centre (<i>WGS 1984</i>)</th><td>{CNTR_LAT}, {CNTR_LON}</td></tr><tr><th>Altitude (<i>ft</i>)</th><td>{ALTITUDE}</td></tr><tr><th>Scale (<i>relative</i>)</th><td>{SCALE}</td></tr><tr><th>Camera</th><td>{CAMERA}</td></tr><tr><th>Lens</th><td>{LENS_NAME}</td></tr><tr><th>Focal Length (<i>mm</i>)</th><td>{FOCAL_LEN}</td></tr><tr><th>Film Type</th><td>{FILM_TYPE}</td></tr><tr><th>Film size (<i>mm</i>)</th><td>{FILM_SIZE}</td></tr><tr><th>Photo Dimensions (<i>in</i>)</th><td>{WIDTH} x {HEIGHT}</td></tr><tr><th>Box</th><td>{BOX}</td></tr><tr><th>File Name</th><td>{PHOTOID}</td></tr><tr style='display:none;'><th>Photo Year</th><td>{Year_}</td></tr><tr style='display:none;'><th>Raster ID</th><td>{RASTERID}</td></tr><tr style='display:none;'><th>Download URL</th><td>{DownloadURL}</td></tr></table>"
 
         const clusterConfig = {
           type: "cluster",
@@ -118,7 +140,7 @@ export default class App extends Component {
         };
 
         const photoCluster = new FeatureLayer({
-          url: "https://madgic.trentu.ca/arcgis/rest/services/NAPLIndex/AirPhoto_Points/MapServer/0",
+          url: "https://madgic.trentu.ca/arcgis/rest/services/airphoto2020/Airphoto2020/MapServer/0",
           outFields: ["*"],
           opacity: 0,
           minScale: 60000,
@@ -135,7 +157,7 @@ export default class App extends Component {
         });
 
         const photoLayer = new FeatureLayer({
-          url: "https://madgic.trentu.ca/arcgis/rest/services/NAPLIndex/AirPhoto_Points/MapServer/0",
+          url: "https://madgic.trentu.ca/arcgis/rest/services/airphoto2020/Airphoto2020/MapServer/0",
           outFields: ["*"],
           opacity: 0.75,
           popupTemplate:{
@@ -163,6 +185,8 @@ export default class App extends Component {
                 fieldName: "STATUS"
               },{
                 fieldName: "NAPL_DATE"
+              },{
+                fieldName: "Collection"
               },{
                 fieldName: "ALTITUDE"
               },{
@@ -226,30 +250,26 @@ export default class App extends Component {
             };
           });
         });
+        
         view.when(function () {
           var popup = view.popup;
           popup.viewModel.on("trigger-action", function (event) {
+            // Attributes used for both Viewing and Downloading photos
+            var attributes = popup.viewModel.selectedFeature.attributes;
+            var photoID = attributes.RASTERID;
+            // Download photo operation to hit the Download URL
             if (event.action.id === "download-photo") {
-              var attributes = popup.viewModel.selectedFeature.attributes;
-              // Get the 'website' field attribute
+              // Get the 'Download URL' field attribute
               var info = attributes.DownloadURL;
-              var photoID = attributes.PHOTOID;
-              // Make sure the 'website' field value is not null
+              // Make sure the 'Download URL' field value is not null
               if (info) {
-                // Open up a new browser using the URL value in the 'website' field
+                // Open up a new browser using the Download URL value
                 window.open(info.trim());
                 console.log(photoID + " successfully downloaded.")
+                // Add error Handling
               }
-            }
-          });
-        });
-
-        view.when(function () {
-          var popup = view.popup;
-          popup.viewModel.on("trigger-action", function (event) {
-            if (event.action.id === "view-photo") {
-              var attributes = popup.viewModel.selectedFeature.attributes;
-              var photoID = attributes.RASTERID;
+            } else if (event.action.id === "view-photo") {
+              // Collect relevant attributes to populate the Service URL for the specific photo
               var layerID = attributes.PHOTOID;
               var yearStr = attributes.Year_;
               var year = parseInt(yearStr);
@@ -259,14 +279,13 @@ export default class App extends Component {
               if (year > 1970) {
                 // Insert Error or disable View button
                 console.log("Viewing is not active for this photo.")
-              }else {
+              } else {
                 const photoView = new ImageryLayer({
                   url:
                     serviceURL,
                     definitionExpression: defExp,
                     title: layerID
                 });
-
                 map.add(photoView, 0);
                 photoView.load().then(function(){
                   console.log(layerID + " added to map successfully.");
@@ -275,6 +294,24 @@ export default class App extends Component {
             }
           });
         });
+
+        function defineActions(event) {
+          var item = event.item;
+          if (item.title === "Airphoto Points") {
+            item.actionsSections = [
+              {
+                title: "Increase opacity",
+                className: "esri-icon-up",
+                id: "increase-opacity"
+              },
+              {
+                title: "Decrease opacity",
+                className: "esri-icon-down",
+                id: "decrease-opacity"
+              }
+            ];
+          }
+        };
 
         // Layer List Widget
         view.when(function () {
@@ -290,12 +327,58 @@ export default class App extends Component {
                   open: false
                 };
               }
+            }, defineActions
+          });
+
+          layerList.on("trigger-action", function (event) {
+            // Capture the action id.
+            var id = event.action.id;
+
+            if (id === "increase-opacity") {
+              if (photoLayer.opacity < 1) {
+                photoLayer.opacity += 0.25;
+              }
+            } else if (id === "decrease-opacity") {
+              if (photoLayer.opacity > 0) {
+                photoLayer.opacity -= 0.25;
+              }
             }
-            // add opacity sliders for photos added to map
           });
           // Add widget to the bottom right corner of the view
           view.ui.add(layerList, "bottom-right");
         });
+
+        //const yearNode = document.querySelectorAll('.year-item');
+        const yearElement = document.getElementById("year-filter");
+
+        yearElement.addEventListener("click", filterByYear);
+
+        function filterByYear(event) {
+          const selectedYear = event.target.getAttribute("data-year");
+          photoLayerView.filter = {
+            where: "Year_ like '" + selectedYear + "%'"
+          };
+          console.log(photoLayerView.filter.where)
+        }
+
+        view.whenLayerView(photoLayer).then(function (layerView) {
+          photoLayerView = layerView;
+          yearElement.style.visibility = "visible";
+          const yearExpand = new Expand({
+            view: view,
+            expandTooltip: "Filter by Decade",
+            collapseTooltip: "Clear Filter",
+            content: yearElement,
+            expandIconClass: "esri-icon-filter",
+            group: "top-left"
+          });
+          yearExpand.watch("expanded", function () {
+            if (!yearExpand.expanded) {
+              photoLayerView.filter = null;
+            }
+          });
+          view.ui.add(yearExpand, "top-left");
+        })
 
         // Standard Home Widget
         const homeWidget = new Home({
@@ -305,65 +388,6 @@ export default class App extends Component {
         view.ui.add(homeWidget, {
           position: "top-left"
         });
-
-        /*var qTask = new QueryTask({
-          url: photoLayer
-        });
-        var params = new Query({
-          returnGeometry: true,
-          outFields: ["*"]
-        });
-
-        view.when(funciton() {
-          view.ui.add("optionsDiv", "bottom-right");
-          document.getElementById("doBtn").addEventListener("click", doQuery);
-        });
-        var yearSel = document.getElementById("yearSelection");
-        var rollSel = document.getElementById("rollSelection");
-        var photoSel =document.getElementById("photoSelection");
-        
-        function doQuery() {
-          resultsLayer.removeAll();
-          params.where = yearSelection.value + rollSelection.value + photoSelection.value;
-          qTask.execute(params).then(getResults).catch(promiseRejected);
-        }
-
-        function getResults(response) {
-          var photoResults = response.features.map(function (feature) {
-            feture.symbol = {
-              type: "point",
-              symbolLayers: [
-                {
-                  ***Define 2D symbol for results***
-                }
-              ]
-            };
-            feture.popupTemplate = popupTemplate;
-            return feature;
-          });
-          resultsLayer.addMany(photoResults);
-          view
-            .goTo(photoResults)
-            .then(funtion () {
-              view.popup.open({
-                features: photoResults,
-                featureMenuOpen: true,
-                updateLocationEnabled: true
-              });
-            })
-            .catch(feature (error) {
-              if (error.name != "AbortError") {
-                console.error(error);
-              }
-            });
-          document.getElementById("printResults").innerHTML =
-            photoResults.length + " results found!";
-        }
-
-        funtion promiseRejected(error) {
-          console.error("Promise rejected: ", error.message);
-        }
-        */
 
         const searchWidget = new Search ({
           view: view,
@@ -404,7 +428,8 @@ export default class App extends Component {
           position: "bottom-left"
         });
 
-        map.reorder(footprintsLayer, 3); // Move Generalized Footprint to top of Layer List
+        map.reorder(footprintsLayer, 4); // Move Generalized Footprint to top of Layer List
+        map.reorder(envelopesLayer, 3);
       })
 
       .catch((err) => {
@@ -423,7 +448,17 @@ export default class App extends Component {
     return (
       <div className='App'>
         <Header />
-        <div id='mapContainer' className="esri-widget"/>
+        <div id='year-filter' className='esri-widget'>
+          <div className='year-item visible-year' data-year='192'>1920s</div>
+          <div className='year-item visible-year' data-year='193'>1930s</div>
+          <div className='year-item visible-year' data-year='194'>1940s</div>
+          <div className='year-item visible-year' data-year='195'>1950s</div>
+          <div className='year-item visible-year' data-year='196'>1960s</div>
+          <div className='year-item visible-year' data-year='197'>1970s</div>
+          <div className='year-item visible-year' data-year='198'>1980s</div>
+          <div className='year-item visible-year' data-year='199'>1990s</div>
+        </div>
+        <div id='mapContainer' className='esri-widget'/>
         <Cookies />
       </div>
     );
@@ -431,3 +466,63 @@ export default class App extends Component {
 }
 
 // logo: <img src={logo} alt="Trent University Library & Archives" /> 
+
+/*** QUERY TASK ***/
+/*var qTask = new QueryTask({
+  url: photoLayer
+});
+var params = new Query({
+  returnGeometry: true,
+  outFields: ["*"]
+});
+
+view.when(funciton() {
+  view.ui.add("optionsDiv", "bottom-right");
+  document.getElementById("doBtn").addEventListener("click", doQuery);
+});
+var yearSel = document.getElementById("yearSelection");
+var rollSel = document.getElementById("rollSelection");
+var photoSel =document.getElementById("photoSelection");
+
+function doQuery() {
+  resultsLayer.removeAll();
+  params.where = yearSelection.value + rollSelection.value + photoSelection.value;
+  qTask.execute(params).then(getResults).catch(promiseRejected);
+}
+
+function getResults(response) {
+  var photoResults = response.features.map(function (feature) {
+    feture.symbol = {
+      type: "point",
+      symbolLayers: [
+        {
+          ***Define 2D symbol for results***
+        }
+      ]
+    };
+    feture.popupTemplate = popupTemplate;
+    return feature;
+  });
+  resultsLayer.addMany(photoResults);
+  view
+    .goTo(photoResults)
+    .then(funtion () {
+      view.popup.open({
+        features: photoResults,
+        featureMenuOpen: true,
+        updateLocationEnabled: true
+      });
+    })
+    .catch(feature (error) {
+      if (error.name != "AbortError") {
+        console.error(error);
+      }
+    });
+  document.getElementById("printResults").innerHTML =
+    photoResults.length + " results found!";
+}
+
+funtion promiseRejected(error) {
+  console.error("Promise rejected: ", error.message);
+}
+*/
